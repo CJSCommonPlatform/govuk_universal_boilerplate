e9f299813ea7e7705f2c84f8ea1ca88cfd6e0fd7
jamesbirrellgray <james@jamesbirrellgray.com>
2016-08-01 10:37:01 +0100

    feat:adding a better ng2 biolerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,105 +1,83 @@
-import { Component, Directive, ElementRef, Renderer } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Http } from '@angular/http';
+/*
+ * Angular 2 decorators and services
+ */
+import { Component, ViewEncapsulation } from '@angular/core';
 
-// templateUrl example
-import { Home } from './home';
-//
-/////////////////////////
-// ** Example Directive
-// Notice we don't touch the Element directly
+import { AppState } from './app.service';
 
-@Directive({
-  selector: '[x-large]'
-})
-export class XLarge {
-  constructor(element: ElementRef, renderer: Renderer) {
-    // ** IMPORTANT **
-    // we must interact with the dom through -Renderer-
-    // for webworker/server to see the changes
-    renderer.setElementStyle(element.nativeElement, 'fontSize', 'x-large');
-    // ^^
-  }
-}
-
-/////////////////////////
-// ** Example Components
+/*
+ * App Component
+ * Top Level Component
+ */
 @Component({
-  selector: 'about',
-  template: `
-    <div>This is the "About" page</div>
-  `
-})
-export class About { }
-
-/////////////////////////
-// ** MAIN APP COMPONENT **
-@Component({
-  selector: 'app', // <app></app>
-  directives: [
-    ...ROUTER_DIRECTIVES,
-    XLarge
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.style.css'
   ],
-  styles: [`
-    * { padding:0; margin:0; }
-    #universal { text-align:center; font-weight:bold; padding:15px 0; }
-    nav { background:#158126; min-height:40px; border-bottom:5px #046923 solid; }
-    nav a { font-weight:bold; text-decoration:none; color:#fff; padding:20px; display:inline-block; }
-    nav a:hover { background:#00AF36; }
-    .hero-universal { min-height:500px; display:block; padding:20px; background: url('/assets/logo.png') no-repeat center center; }
-    .inner-hero { background: rgba(255, 255, 255, 0.75); border:5px #ccc solid; padding:25px; }
-    .router-link-active { background-color: #00AF36; }
-    blockquote { border-left:5px #158126 solid; background:#fff; padding:20px 20px 20px 40px; }
-    blockquote::before { left: 1em; }
-    main { padding:20px 0; }
-    pre { font-size:12px; }
-  `],
   template: `
-  <h3 id="universal">Angular2 Universal</h3>
-  <nav>
-    <a [routerLinkActive]="['active', 'router-link-active']" [routerLink]=" ['./home'] ">Home</a>
-    <a [routerLinkActive]="['active', 'router-link-active']" [routerLink]=" ['./about'] ">About</a>
-  </nav>
-  <div class="hero-universal">
-    <div class="inner-hero">
+    <nav>
+      <span>
+        <a [routerLink]=" ['./'] ">
+          Index
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./home'] ">
+          Home
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./detail'] ">
+          Detail
+        </a>
+      </span>
+      |
+      <span>
+        <a [routerLink]=" ['./about'] ">
+          About
+        </a>
+      </span>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+
+    <footer>
+      <span>WebPack Angular 2 Starter by <a [href]="url">@AngularClass</a></span>
       <div>
-        <span x-large>Universal JavaScript {{ title }}!</span>
+        <a [href]="url">
+          <img [src]="angularclassLogo" width="25%">
+        </a>
       </div>
-
-      Two-way binding: <input type="text" [value]="title" (input)="title = $event.target.value" autofocus>
-      <br><br>
-
-      <strong>Async data call return value:</strong>
-      <pre>{{ data | json }}</pre>
-
-      <strong>Router-outlet:</strong>
-      <main>
-        <router-outlet></router-outlet>
-      </main>
-
-      <blockquote>{{ server }}</blockquote>
-    </div>
-  </div>
+    </footer>
   `
 })
 export class App {
-  title: string = 'ftw';
-  data = {};
-  server: string;
+  angularclassLogo = 'assets/img/angularclass-avatar.png';
+  name = 'Angular 2 Webpack Starter';
+  url = 'https://twitter.com/AngularClass';
 
-  constructor(public http: Http) { }
+  constructor(
+    public appState: AppState) {
+
+  }
 
   ngOnInit() {
-    // limit the use of setTimeouts
-    setTimeout(() => {
-      this.server = 'This was rendered from the server!';
-    }, 10);
-
-    // use services for http calls
-    this.http.get('/data.json')
-      .subscribe(res => {
-        this.data = res.json();
-      });
+    console.log('Initial App State', this.appState.state);
   }
 
 }
+
+/*
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,16 +1,59 @@
-// Angular 2 Universal
+/*
+ * Providers provided by Angular
+ */
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { provideRouter } from '@angular/router';
-import { HTTP_PROVIDERS } from '@angular/http';
+/*
+* Platform and Environment
+* our providers/directives/pipes
+*/
+import { PLATFORM_PROVIDERS } from './platform/browser';
+import { ENV_PROVIDERS, decorateComponentRef } from './platform/environment';
 
-// Application
-import {App} from './app/app.component';
-import {routes} from './app/app.routes';
 
-// you must return bootstrap for client.ts
-export function ngApp() {
+/*
+* App Component
+* our top level component that holds all of our components
+*/
+import { App, APP_PROVIDERS } from './app';
+
+/*
+ * Bootstrap our Angular app with a top level component `App` and inject
+ * our Services and Providers into Angular's dependency injection
+ */
+export function main(initialHmrState?: any): Promise<any> {
+
   return bootstrap(App, [
-    ...HTTP_PROVIDERS,
-    provideRouter(routes)
-  ]);
+    // To add more vendor providers please look in the platform/ folder
+    ...PLATFORM_PROVIDERS,
+    ...ENV_PROVIDERS,
+    ...APP_PROVIDERS,
+  ])
+  .then(decorateComponentRef)
+  .catch(err => console.error(err));
+
+}
+
+
+
+
+
+/*
+ * Vendors
+ * For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
+ * You can also import them in vendors to ensure that they are bundled in one file
+ * Also see custom-typings.d.ts as you also need to do `typings install x` where `x` is your module
+ */
+
+
+/*
+ * Hot Module Reload
+ * experimental version by @gdi2290
+ */
+if ('development' === ENV && HMR === true) {
+  // activate hot module reload
+  let ngHmr = require('angular2-hmr');
+  ngHmr.hotModuleReplacement(main, module);
+} else {
+  // bootstrap when document is ready
+  document.addEventListener('DOMContentLoaded', () => main());
 }
