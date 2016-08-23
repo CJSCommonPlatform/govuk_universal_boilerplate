a9e440a5f2fa5b43c1295e36b21589746d1f2b10
Fabio Tisci <fabio.tapia@ext.soprasteria.com>
2016-08-23 15:37:02 +0100

    removed unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, Directive, ElementRef, Renderer, ViewEncapsulation } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Http } from '@angular/http';
 
 // templateUrl example
 import { Home } from './home';
@@ -105,23 +104,5 @@ export class About { }
   `
 })
 export class App {
-  title: string = 'ftw';
-  data = {};
-  server: string;
-
-  constructor(public http: Http) { }
-
-  ngOnInit() {
-    // limit the use of setTimeouts
-    setTimeout(() => {
-      this.server = 'This was rendered from the server!';
-    }, 10);
-
-    // use services for http calls
-    this.http.get('/data.json')
-      .subscribe(res => {
-        this.data = res.json();
-      });
-  }
 
 }
